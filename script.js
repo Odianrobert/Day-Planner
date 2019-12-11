fd75534f3252648b0f203d515dcebba1b7e039cd
Bobby Odian <odianrobert@yahoo.com>
2019-12-10 22:31:14 -0500

    added click to save button, console worked

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
+//Display current day on day planner
 let currentDay = document.querySelector ("#currentDay")
 
 currentDay = moment().format("dddd, MMMM, Do");
 $("#currentDay").append(currentDay)
+
+// add listener for save button
+// set up variables to be saved on buttton click
+//set values to local storage
+$(document).ready(function() {
+
+    $(".saveBtn").on("click", function() {
+      var value = $(this).siblings(".description").val();
+      var time = $(this).parent().attr("id");
+      //console.log("click");
+      
+      localStorage.setItem(time, value);
+      //console.log(time, value);
+    });
+});
+
+
+
+
+   
